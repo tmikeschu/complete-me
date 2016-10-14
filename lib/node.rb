15e75543f619b1e2d7c5f09f5f810718f9d2fff1
Mike Schutte <tmikeschutte@gmail.com>
2016-10-13 22:26:46 -0600

    completes working insert method

--- lib/node.rb
+++ lib/node.rb
@@ -1,5 +1,70 @@
 require 'pry'
 
 class Node
+  attr_accessor :links
 
+  def initialize(terminal = false)
+    @links = []
+    @terminal = terminal
+    @count = 0
+  end
+
+  def letters_array(letters)
+    if letters.is_a? String
+      letters.chars
+    else
+      letters
+    end
+  end
+
+  def insert(letters)
+    letters = letters_array(letters)
+    first   = letters.shift
+    insert_decision(first, letters)
+  end
+  
+  def insert_decision(first, letters)
+    #binding.pry
+    if letters.empty?
+      add_link(first, new_terminal_node)
+    elsif letter_already_inserted?(first)
+      next_link(first).insert(letters)
+    else
+      add_link_and_move_to_next_letter(first, letters)
+    end
+  end
+
+  def letter_already_inserted?(first)
+    if links.any?
+      already_inserted_letters.include?(first)
+    else
+      false
+    end
+  end
+
+  def already_inserted_letters
+    inserted_letters = links.map { |link| link.keys}.flatten
+  end
+
+  def next_link(first)
+    link_of_letter(first).values.first
+  end
+
+  def link_of_letter(first)
+    link = links.find {|link| link.keys[0] == first}
+  end
+  
+  def add_link(letter, node)
+    @links << {letter => node}
+  end
+  
+  def new_terminal_node 
+    Node.new(terminal = true)
+  end
+
+  def add_link_and_move_to_next_letter(first, letters)
+      new_node = Node.new
+      @links << {first => new_node}
+      new_node.insert(letters)
+    end
 end
--- test/node_test.rb
+++ test/node_test.rb
@@ -5,9 +5,37 @@ require './lib/node'
 require 'pry'
 
 class NodeTest < Minitest::Test
+
   def test_it_exists
     assert Node.new
   end
+
+  def test_it_inserts_one_letter
+    root = Node.new
+    root.insert("b")
+    root_link = root.links.first.keys
+    assert_equal ["b"], root_link
+  end
+
+  def test_it_inserts_two_letters
+    root = Node.new
+    root.insert("bo")
+    second_link = root.links.first.values.first.links.first.keys
+    assert_equal ["o"], second_link
+  end
+
+  def test_it_skips_prefix_letters_if_already_inserted
+    binding.pry
+    root = Node.new
+    root.insert("car")
+    root.insert("carts")
+    root.insert("casted")
+    root.insert("bust")
+    root.insert("busted")
+    root.insert("flower")    
+    #assert that keys are different and unique
+  end
+
 
   
 end
