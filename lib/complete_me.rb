781ffdec5ca10b6e33eac168e3c585c0596ad651
Mike Schutte <tmikeschutte@gmail.com>
2016-10-15 09:33:17 -0600

    adds functionality and inheritance to CompleteMe class

--- lib/complete_me.rb
+++ lib/complete_me.rb
@@ -1,5 +1,12 @@
 require './lib/node'
 
-class CompleteMe
+class CompleteMe < Node
+  attr_reader :root
+  def initialize
+    @root = Node.new
+  end
 
+  def insert(word)
+    @root.insert(word)
+  end
 end
